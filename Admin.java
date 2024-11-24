5fa20a71c22c98fc22cb3a592a7170ee5d670cb8
Fadhlallah Almohammed <fadilullah17@gmail.com>
2024-11-24 22:17:33 +0300

    update 1

---
+++ Admin.java
@@ -0,0 +1,19 @@
+package Vehicle_Rental_System;
+
+public class Admin {
+    
+
+    private VehicleManager vehicleManager;
+
+
+    Admin() {
+
+    }
+
+
+    public boolean isAuthorized(boolean isLoggedIn, String authorizeCode) {
+
+        return true;
+    }
+    
+}
--- VehicleManager.java
+++ VehicleManager.java
@@ -6,8 +6,19 @@ package Vehicle_Rental_System;
 
 public class VehicleManager {
 
+
+    private static final String AUTHORIZE_CODE = "";
     public static void main(String[] args) {
         
         
     }
+
+
+
+
+    public boolean isAuthorized(boolean isLoggedIn, String authorizeCode) {
+
+        return true;
+    }
+
 }
